9c89799e63683c39b3e55565515d8b34b7f1b0d0
Александр <hyperpositron@gmail.com>
2023-01-31 13:33:09 +0200

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         int year = 2023;
-         printInfoAboutYear(year);
+        System.out.println("Задача №1");
+        printInfoAboutYear(year);                          //task1
+        System.out.println("Задача №2");
+        printInfoAboutVersion(1,2022);     //task2
 
+    }
+
+    private static void printInfoAboutVersion(int os, int productionYear) {
+        if (os < 0 || os > 1) {
+            System.out.println("OC долдна быть ровна 0 или 1 ");
+            return;
+        }
+        int currentYear = LocalDate.now().getYear();
+        StringBuilder result = new StringBuilder("Установите ");
+        if (productionYear < currentYear) {
+            result.append("облегченную версию для ");
+        } else {
+            result.append(" версию для ");
+        }
+        if (os == 0) {
+            result.append("iOS");
+        } else {
+            result.append("Android");
+        }
+        System.out.println(result);
     }
 
     private static void printInfoAboutYear(int year) {
